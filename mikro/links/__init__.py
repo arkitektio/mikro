ff45eea482cad01f50417f1c27956879229c987c
jhnnsrs <jhnnsrs@gmail.com>
2022-03-18 13:26:10 +0100

    added documentaiton first

--- mikro/links/__init__.py
+++ mikro/links/__init__.py
@@ -1,2 +1,10 @@
+"""Extra Links for Mikro
+
+Implements both XArray Upload Lin and Parquet Upload Link
+
+
+"""
+
+
 from .xarray import DataLayerXArrayUploadLink
 from .parquet import DataLayerParquetUploadLink
